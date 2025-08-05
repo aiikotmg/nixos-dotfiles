77faa5f1232f4cb5218aa22502394a84a797edf6
aiikotmg <sslerror@protonmail.com>
2025-08-05 00:30:54 -0500

    changed foot config

--- hm-mod/foot/default.nix
+++ hm-mod/foot/default.nix
@@ -10,31 +10,32 @@ in {
             enable = true;
             settings = {
                 main = {
-                    font = "JetBrains Mono Nerdfont:size=7:line-height=16px";
-                    pad = "12x12";
+                    shell = "zsh";
+                    font = "0xProto Nerd Font Mono:size=10";
+                    pad = "6x6";
                 };
-                colors = with config.colorScheme.palette; {
-                    foreground = "${base06}";
-                    background = "${base00}";
-                    ## Normal/regular colors (color palette 0-7)
-                    regular0="${base00}";  # black
-                    regular1="${base08}";  # red
-                    regular2="${base0B}";  # green
-                    regular3="${base0A}";  # yellow
-                    regular4="${base0D}";  # blue
-                    regular5="${base0E}";  # magenta
-                    regular6="${base0C}";  # cyan
-                    regular7="${base06}";  # white
+#                colors = with config.colorScheme.palette; {
+#                    foreground = "${base06}";
+#                    background = "${base00}";
+#                    ## Normal/regular colors (color palette 0-7)
+#                    regular0="${base00}";  # black
+#                    regular1="${base08}";  # red
+#                    regular2="${base0B}";  # green
+#                    regular3="${base0A}";  # yellow
+#                    regular4="${base0D}";  # blue
+#                    regular5="${base0E}";  # magenta
+#                    regular6="${base0C}";  # cyan
+#                    regular7="${base06}";  # white
 
-                    bright0="${base00}"; # bright black
-                    bright1="${base08}"; # bright red
-                    bright2="${base0B}";# bright green
-                    bright3="${base09}";# bright yellow
-                    bright4="${base0D}";
-                    bright5="${base0E}";# bright magenta
-                    bright6="${base0C}";# bright cyan
-                    bright7="${base06}";# bright white
-                };
+#                    bright0="${base00}"; # bright black
+#                    bright1="${base08}"; # bright red
+#                   bright2="${base0B}";# bright green
+#                    bright3="${base09}";# bright yellow
+#                    bright4="${base0D}";
+#                    bright5="${base0E}";# bright magenta
+#                    bright6="${base0C}";# bright cyan
+#                    bright7="${base06}";# bright white
+#                };
             };
         };
     };

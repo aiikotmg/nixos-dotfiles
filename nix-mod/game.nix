11f307e2bf6270b97d61e0953558bbd9367476ca
aiikotmg <sslerror@protonmail.com>
2025-12-09 19:23:58 -0600

    oversteer

--- nix-mod/game.nix
+++ nix-mod/game.nix
@@ -3,11 +3,6 @@
 {
 
   #gamestuff
-  # some games on #hermes require PROTON_USE_WINED3D=1 MESA_LOADER_DRIVER_OVERRIDE=crocus %command%
-  #
-  # TODO: get this working right VV
-  # gamescope -e -- bash -c " gamemoderun  %command% &; mangoapp"
-  # both lines go in the launch options of the game in steam
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
   programs.gamemode.enable = true;
@@ -16,6 +11,7 @@
 
     dotnetCorePackages.aspnetcore_9_0-bin
 #    dotnetCorePackages.aspnetcore-runtime_9_0
+    oversteer
     mangohud
     wine
   ];
